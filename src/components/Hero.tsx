68ec6ad43a318e859341773fd80d3c61f6750457
Tomer Kizel <tomerkizel@gmail.com>
2026-09-05 08:54:59 +0300

    Update brand casing and tab title
    Wordmark: "Orca ONE" -> "Orca One". Tab title -> "Orca One - One Team".

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -158,7 +158,7 @@ export function Hero() {
       <header className="site-header">
         <div className="brand">
           <img src={orcaLogo} alt="Orca logo" className="brand-mark" />
-          <span className="brand-name">Orca ONE</span>
+          <span className="brand-name">Orca One</span>
         </div>
 
         <button
